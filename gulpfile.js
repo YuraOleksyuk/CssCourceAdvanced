619c8e6fbd74af2d5cb18ce680c711f090faa0c7
Юра Олексюк <yuriy.olexiuk@gmail.com>
2018-06-15 16:41:08 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-var sassFiles = 'assets/styles/sass/**/*.scss',
-    cssDest = 'assets/styles/css/';
+var sassFiles = 'sass/**/*.scss',
+    cssDest = 'css';
 
 gulp.task('styles', function(){
     gulp.src(sassFiles)
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest(cssDest));
+});
+
+gulp.task('watch',function() {
+    gulp.watch(sassFiles,['styles']);
 });
